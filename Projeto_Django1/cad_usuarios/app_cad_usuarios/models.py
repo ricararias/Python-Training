c299ca662d8cffdcb137d8f124a5772e6a6187e5
ricararias <88516141+ricararias@users.noreply.github.com>
2023-11-16 15:53:02 -0300

    Projeto_Django1

---
+++ Projeto_Django1/cad_usuarios/app_cad_usuarios/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class  Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nome = models.TextField(max_length=255)
+    idade = models.ImageField()
+
+
+
---
+++ Projeto_Django1/cad_usuarios/app_cad_usuarios/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import Usuario
+
+def home(request):
+    return render(request, 'usuarios/home.html')
+
+def usuarios(request):
+    #Salvar os dados da tela para o banco de dados
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+
+    # Exibir todos os usuarios já cadastrados em uma nova página
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+
+    }
+
+    #Retornar os dados para a página de listagem de usuários
+    return render(request, 'usuarios/usuarios.html',usuarios)
+
+    
+
+
